c3555feb4e5ab04cb9811f8a3c9f488dbc74369b
Antoine <141731510+antoinep23@users.noreply.github.com>
2024-06-11 22:30:28 +0100

    Try to fix auth bug 0.8

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure,
+    // secure: req.secure,
   };
 
   res.cookie('jwt', token, cookieOptions);
